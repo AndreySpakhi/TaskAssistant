1ead1a1c2a0a63a1f8eec6840f40947c68fd8ec0
AndreySpakhi <andreyspakhi@ukr.net>
2022-06-27 15:35:16 +0300

    last step

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,21 +8,46 @@ import Signup from './components/signup';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Navbar';
+import TodoDataService from './services/todos';
 function App() {
   const [user, setUser] = React.useState(null);
   const [token, setToken] = React.useState(null);
   const [error, setError] = React.useState('');
 
   async function login(user = null){ // default user to null
-    setUser(user);
+    TodoDataService.login(user)
+      .then(response => {
+        setToken(response.data.token);
+        setUser(user.username);
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('user', user.username);
+        setError('');
+      })
+      .catch(e => {
+        console.log('login', e);
+        setError(e.toString());
+      });
   }
 
   async function logout(){
-    setUser(null);
+    setToken('');
+    setUser('');
+    localStorage.setItem('token', '');
+    localStorage.setItem('user', '');
   }
 
   async function signup(user = null){ // default user to null
-    setUser(user);
+    TodoDataService.signup(user)
+      .then(response => {
+        setToken(response.data.token);
+        setUser(user.username);
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('user', user.username);
+      })
+      .catch(e => {
+        console.log(e);
+        setError(e.toString());
+      })
   }
   return (
     <div className="App">
@@ -33,7 +58,7 @@ function App() {
             <Container Container>
               <Link class="nav-link" to={"/todos"}>Todos</Link>
               {user ? (
-                <Link class="nav-link">Logout ({user})</Link>
+                <Link class="nav-link" onClick={logout}>Logout ({user})</Link>
               ) : (
                 <>
                   <Link class="nav-link" to={"/login"}>Login</Link>
@@ -71,19 +96,7 @@ function App() {
       <footer className="text-center text-lg-start 
         bg-light text-muted mt-4">
         <div className="text-center p-4">
-          © Copyright - <a 
-            target="_blank" 
-            className="text-reset fw-bold text-decoration-none" 
-            href="https://twitter.com/greglim81"
-          >
-            Greg Lim
-          </a> - <a 
-            target="_blank" 
-            className="text-reset fw-bold text-decoration-none" 
-            href="https://twitter.com/danielgarax"
-          >
-            Daniel Correa
-          </a>
+          © Copyright - Greg Lim - Daniel Correa
         </div>
       </footer>
     </div>
